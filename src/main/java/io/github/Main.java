26080e837db6ffdb674fd0ed65bc50dd4fdd4dec
Douglas Liebl <douglasliebl@outlook.com>
2023-12-04 16:29:15 -0300

    Add: main page test

--- src/main/java/io/github/Main.java
+++ src/main/java/io/github/Main.java
@@ -1,12 +1,15 @@
 package io.github;
 
 import io.github.configuration.DatabaseConnection;
+import io.github.pages.MainPage;
 
 import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
 
+        MainPage mainPage = new MainPage();
+        mainPage.setVisible(true);
 
         // Database Connection test
         Connection connection = null;
